6e97eb3eaf3194577b1443fd2da841bd2b5743d1
BH94 <ybh942002@gmail.com>
2020-09-20 19:30:08 +0900

    [STEP.2] 컴포넌트 코드 스플리팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-// import notify from "./notify";//notify 코드가 main 파일안에 들어가게 된다.
-//import() 함수 형태로 메서드 안에서 사용하면 파일을 분리시켜 저장할 수 있다.
-//실체 필요한 지점에서 파일을 불러와서 함수를 사용할 수 있다.
 
-function App() {
-  const onClick = () => {
-    import("./notify").then((result) => result.default());
+class App extends Component {
+  state = {
+    SplitMe: null,
   };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-      </header>
-    </div>
-  );
+  handleClick = async () => {
+    const loadedModule = await import("./components/SplitMe"); //컴포넌트 관련 코드를 불러온다.
+    //매번 state를 만들어주어야 한다.
+    this.setState({
+      SplitMe: loadedModule.default,
+    });
+  };
+
+  render() {
+    const { SplitMe } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handleClick}>Hello React!</p>
+          {SplitMe && <SplitMe />}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/SplitMe.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SplitMe(props) {
+  return <div>SplitMe!</div>;
+}
+
+export default SplitMe;
